49f163ac6a8619b210150c3971788108ee74fcfe
Vincent Lu <vincenttlu2005@gmail.com>
2026-04-24 15:52:59 -0700

    Added first version of main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void addTask(void (*func)());
+void schedule();
+void runCurrentTask();
+
+void task1(){
+    cout << "Task 1 executing\n";
+};
+
+void task2(){
+    cout << "Task 2 executing\n";
+};
+
 int main() {
-    cout << "Hello, start of repo!" << endl;
+    addTask(task1);
+    addTask(task2);
+
+    while (true){
+        schedule();
+        runCurrentTask();
+    }
+
     return 0;
 }
